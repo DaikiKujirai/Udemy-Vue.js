ea57c43a3068a5d61d7a31a710e4436c9affd195
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-08 22:07:41 +0900

    section8

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+// コンポーネントのオブジェクトをインポートする。
+// Appはコンポーネントのオブジェクトになる
+import LikeNumber from "./components/LikeNumber.vue"
+
+Vue.config.productionTip = false
+Vue.component('LikeNumber', LikeNumber);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
