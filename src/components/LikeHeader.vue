87699a8595c06aeb078de46db75933f92e0dca73
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-10 16:38:17 +0900

    section9

--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -1,11 +1,25 @@
 <template>
-  <h1>{{ headerText }}</h1>
+  <div>
+    <slot></slot>
+    <!--v-bind:user  -->
+    <slot></slot>
+    <hr>
+    <p>いいねの数</p>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 
 export default {
-  props: ["headerText"]
+  deta() {
+    return {
+      user: {
+        firstName: "Jack",
+        lastName: "Donald"
+      }
+    };
+  }
 };
 
 </script>
