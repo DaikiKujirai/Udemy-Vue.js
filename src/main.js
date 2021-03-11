76ddc0e35361f3d3dfdde19f567d1681e8107940
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-11 20:26:00 +0900

    150

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-// コンポーネントのオブジェクトをインポートする。
-// Appはコンポーネントのオブジェクトになる
 import LikeNumber from "./components/LikeNumber.vue"
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
 Vue.filter("upperCase", function(value) {
-  // 第一引数:名前 第二引数:関数
   return value.toUpperCase();
 });
 
